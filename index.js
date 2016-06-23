15368547b9232ecf1b6f28612534c93c0ca1d1e0
Kohei TAKATA <kt.koheitakata@gmail.com>
2016-06-23 13:03:52 +0900

    Understand how to use router

--- index.js
+++ index.js
@@ -12,16 +12,23 @@ const mainView = (params, state, send) =>
     choo.view`
       <main>
         <h1>${state.title}</h1>
-        <input type="text" oninput=${(e) => send('update', {value: e.target.value})}>
+        <input type="text" oninput=${(e) => send('update', {value: e.target.value})} autofocus />
+        <p><a href="/test">test</a></p>
       </main>
     `;
 
-const testView = (params, state, send) => choo.view`<main><h1>test</h1></main>`;
+const testView = (params, state, send) =>
+    choo.view`
+      <main>
+        <h1>test</h1>
+        <p><a href="/">HOME</a></p>
+      </main>
+    `;
 
 app.router((route) => [
     route('/', mainView),
     route('/test', testView)
-  ]);
+]);
 
 const tree = app.start();
 document.body.appendChild(tree);
